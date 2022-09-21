bd7ef1aa58864e2f33b4e27b4e323958bfca7f4f
mrsins <amin.d.d.y1357@gmail.com>
2022-09-22 00:44:36 +0330

    Register system created.(Whitout email verification.)

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 
@@ -15,3 +17,54 @@ class LoginForm(forms.Form):
         label="رمز عبور"
     )
 
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput,
+        max_length=32,
+        min_length=4,
+        label="نام کاربری",
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput,
+        label="ایمیل",
+        help_text="مثال: example@example.com"
+    )
+    password = forms.CharField(
+        widget=forms.TextInput,
+        max_length=32,
+        min_length=8,
+        label="رمز عبور",
+        help_text="رمز عبور باید ترکیبی از حروف کوچک, حروف بزرگ و اعداد باشد."
+    )
+    repeat_password = forms.CharField(
+        widget=forms.TextInput,
+        max_length=32,
+        min_length=8,
+        label="تکرار رمز عبور"
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        have_upper = re.findall("[A-Z]", str(password))
+        if not have_upper:
+            raise forms.ValidationError("رمز باید ترکیبی از اعداد, حروف بزرگ و حروف کوچک باشد!")
+        have_digit = re.findall("[0-9]", str(password))
+        if not have_digit:
+            raise forms.ValidationError("رمز باید ترکیبی از اعداد, حروف بزرگ و حروف کوچک باشد!")
+        return password
+
+    def clean_repeat_password(self):
+        password = self.cleaned_data.get("password")
+        repeat_password = self.cleaned_data.get("repeat_password")
+        if password != repeat_password:
+            raise forms.ValidationError("رمز عبور با تکرار رمز یکی نیست!")
+        return repeat_password
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import get_user_model, login, logout, authenticate
@@ -5,7 +6,8 @@ from django.urls import reverse_lazy
 from django.views.generic.base import View
 from django.views.generic.edit import FormView
 
-from users.forms import LoginForm
+from users.forms import LoginForm, RegisterForm
+from users.models import User
 
 user = get_user_model()
 
@@ -27,3 +29,35 @@ class LoginView(FormView):
             return render(self.request, "user/login.html", {
                 "form": form,
             })
+
+
+class RegisterView(FormView):
+    template_name = "user/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        username = form.clean_username()
+        email = form.clean_email()
+        password = form.clean_password()
+
+        # checking username
+        username_check = User.objects.filter(username__exact=username)
+        if username_check.exists():
+            form.add_error("username", "این نام کاربری از قبل موجود است!")
+            return render(self.request, "user/register.html", {
+                "form": form,
+            })
+
+        # checking email
+        email_check = User.objects.filter(email__exact=email)
+        if email_check.exists():
+            form.add_error("email", "این ایمیل از قبل موجود است!")
+            return render(self.request, "user/register.html", {
+                "form": form,
+            })
+        new_user = User.objects.create(username=username, email=email)
+        new_user.set_password(password)
+        new_user.save()
+        # todo:send email
+        return super(RegisterView, self).form_valid(form)
