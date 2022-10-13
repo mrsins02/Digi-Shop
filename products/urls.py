b47d90ce44baf78a164c41ab92f9860934077afb
mrsins <amin.d.d.y1357@gmail.com>
2022-10-13 20:42:24 +0330

    Category url corrected

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name="shop"),
-    path('/categories/<slug:category>', ProductListView.as_view(), name="categories"),
+    path('categories/<slug:category>', ProductListView.as_view(), name="categories"),
     path('products/<slug:slug>/', ProductDetailView.as_view(), name="product_detail"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT + "media/")
